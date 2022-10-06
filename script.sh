eea959de55179c2f18b7b2bf2f9b805cf4a8167b
alemd01 <aaleemd11@gmail.com>
2022-10-06 14:10:13 +0200

    cambios automatizado

--- script.sh
+++ script.sh
@@ -1,13 +1,11 @@
 
 #genero la página estática con los cambios.
-npx @11ty/eleventy --pathprefix='/home/alemd/Github/alemd-11ty' 2>/dev/null
-echo "generada la página estática..."
+npx @11ty/eleventy --pathprefix='/home/alemd/Github/alemd-11ty'
+
 #copio la página estática generada al repositorio github
-cp -r /home/alemd/Github/alemd-11ty/_site/* /home/alemd/Github/alemd-cutreblog 2>/dev/null
-echo "actualizado el repositorio estático..."
+cp -r /home/alemd/Github/alemd-11ty/_site/* /home/alemd/Github/alemd-cutreblog
+
 #Realizamos los cambios en github
-git add . 2>/dev/null
-git commit -am "cambios automatizado" 2>/dev/null
-git push 2>/dev/null
-echo "Actualizada la página"
-echo "fin."
+git add .
+git commit -am "cambios automatizado"
+git push
